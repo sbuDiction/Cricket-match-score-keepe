e79e3465ff516503c073a8d5bc6cbff5edf9f4e9
sbuDiction <diction1997@gmail.com>
2019-09-20 13:03:57 +0200

    added test cases for the app

--- function/score-keeper.js
+++ function/score-keeper.js
@@ -1,7 +1,56 @@
 module.exports = function() {
-  const addScore = str => {};
+  let total = 0;
+  let over = 6;
+  let wickets = 10;
+  let index0 = 0;
+  let index1 = 0;
+  let index2 = 0;
+  let index3 = 0;
+  let index4 = 0;
+  let index5 = 0;
+  let index6 = 0;
+
+  const addScore = str => {
+    var score = str.split(",");
+    if (score.length === 6) {
+      over--;
+      for (let x = 0; x < score.length; x++) {
+        if (score[x] === "-") {
+          index0 += 0;
+        } else if (score[x] === "1") {
+          index1 += 1;
+        } else if (score[x] === "2") {
+          index2 += 2;
+        } else if (score[x] === "3") {
+          index3 += 3;
+        } else if (score[x] === "4") {
+          index4 += 4;
+        } else if (score[x] === "6") {
+          index5 += 6;
+        } else if (score[x] === "w") {
+          wickets--;
+        }
+      }
+    }
+  };
+
+  const displayOver = () => {
+    return over;
+  };
+
+  const displayWickets = () => {
+    return wickets;
+  };
+
+  const getTotal = () => {
+    total = index0 + index1 + index2 + index3 + index4 + index5 + index6;
+    return total;
+  };
 
   return {
-    addScore
+    addScore,
+    displayOver,
+    getTotal,
+    displayWickets
   };
 };
--- test/score-keeper.test.js
+++ test/score-keeper.test.js
@@ -1,9 +1,33 @@
 const assert = require("assert");
 const scoreKeeper = require("../function/score-keeper");
 describe("Score keeper Tests", function() {
-  it("should be able to take in score ", function() {
+  it("should be able to show the total 16 if score was '(1,2,3,4,-,6)' ", function() {
     let score = scoreKeeper();
-
-    assert.equal(score.addScore("-"), "");
+    score.addScore("1,2,3,4,-,6");
+    assert.equal(score.getTotal(), 16);
+  });
+  it("should be able to take a score in this patern '(1,2,3,4,6,-)' ", function() {
+    let score = scoreKeeper();
+    score.addScore("1,2,3,4,6,-");
+    assert.equal(score.getTotal(), 16);
+  });
+  it("should be able to show the over left if the first over has been played ", function() {
+    let score = scoreKeeper();
+    score.addScore("1,2,3,4,6,-");
+    assert.equal(score.displayOver(), 5);
+  });
+  it("should be able to show the over left if 3 games were played ", function() {
+    let score = scoreKeeper();
+    score.addScore("1,2,3,4,6,-");
+    score.addScore("1,2,3,4,6,-");
+    score.addScore("1,2,3,4,6,-");
+    assert.equal(score.displayOver(), 3);
+  });
+  it("should be able to show how many wickets left if 3 of them were made ", function() {
+    let score = scoreKeeper();
+    score.addScore("1,2,3,4,w,-");
+    score.addScore("1,2,3,w,6,-");
+    score.addScore("1,w,3,4,6,-");
+    assert.equal(score.displayWickets(), 7);
   });
 });
